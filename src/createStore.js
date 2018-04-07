5148156122c5bfb117576ee683326d248bb70f3e
yshr446 <yshr446@gmail.com>
2018-04-07 22:27:52 +0900

    Add API package

--- src/createStore.js
+++ src/createStore.js
@@ -1,26 +1,24 @@
 import {
-    createStore as reduxCreateStore,
-    combineReducers,
-    applyMiddleware
+  createStore as reduxCreateStore,
+  combineReducers,
+  applyMiddleware
 } from 'redux';
-
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
 
 import * as reducers from './reducers';
 
-// historyはsrc/index.jsから渡すようにする
 export default function createStore(history) {
-    return reduxCreateStore(
-        combineReducers({
-            ...reducers,
-            // react-router-reduxのReducer
-            router: routerReducer,
-        }),
-        applyMiddleware(
-            logger,
-            // react-router-reduxのRedux Middleware
-            routerMiddleware(history)
-        )
-    );
+  return reduxCreateStore(
+    combineReducers({
+      ...reducers,
+      router: routerReducer,
+    }),
+    applyMiddleware(
+      logger,
+      thunk,
+      routerMiddleware(history)
+    )
+  );
 }
